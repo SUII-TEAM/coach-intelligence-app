88992957f7c53695fbc05cfdff7970fb22428fc3
Habeba Elhosary <habebaelhosary98@gmail.com>
2025-03-22 00:11:48 +0200

    change app name and icon

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:ai_tactical_assistant/firebase_options.dart';
 import 'package:ai_tactical_assistant/my_app.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_notifications_handler/firebase_notifications_handler.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -29,23 +30,16 @@ void main() async {
       fallbackLocale: const Locale('ar'),
       startLocale: const Locale('ar'),
       assetLoader: const RootBundleAssetLoader(),
-      child:
-          // FirebaseNotificationsHandler(
-          //   onTap: (details) {
-          //     // Map<String, dynamic> body = details.firebaseMessage.data;
-          //     // log(body['msg']);
-          //     // sl<AppNavigator>().navigatorKey.currentState?.push(MaterialPageRoute(
-          //     //       builder: (context) => Scaffold(),
-          //     //     ));
-          //   },
-          //   localNotificationsConfiguration: LocalNotificationsConfiguration(
-          //     androidConfig: AndroidNotificationsConfig(
-          //       appIconGetter: (p0) => '@mipmap/launcher_icon',
-          //     ),
-          //   ),
-          // child:
-          const MyApp(),
-      // ),
+      child: FirebaseNotificationsHandler(
+        onTap: (details) {
+          // Map<String, dynamic> body = details.firebaseMessage.data;
+          // log(body['msg']);
+          // sl<AppNavigator>().navigatorKey.currentState?.push(MaterialPageRoute(
+          //       builder: (context) => Scaffold(),
+          //     ));
+        },
+        child: const MyApp(),
+      ),
     ),
   );
 }
